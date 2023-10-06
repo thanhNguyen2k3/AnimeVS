3165346c7cac7661f9a198efdf2d7571f3b3fb65
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-10-06 16:39:33 +0700

    wtf

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://anime-vs.vercel.app' : 'http://localhost:3000'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://anime-vs.vercel.app/api' : 'http://localhost:3000/api'
 });
 
 export default instance;

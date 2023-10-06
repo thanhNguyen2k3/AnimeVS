1d5ad1f3b25d91bf6eeb826e83b792d38e966afc
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-10-06 16:07:11 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -3,18 +3,27 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         NEXTAUTH_SECRET: 'This is an example',
-        GOOGLE_ID: '488893324329 - ceh1spjb11mfkjhdt7toghr55tgkamnb.apps.googleusercontent.com',
-        GOOGLE_SECRET: 'GOCSPX - XtGQwJDJezOrcybvq - pUbDeBUFlh',
+        GOOGLE_ID: '488893324329-ceh1spjb11mfkjhdt7toghr55tgkamnb.apps.googleusercontent.com',
+        GOOGLE_SECRET: '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',
         NEXTAUTH_URL: 'http://localhost:3000',
         NEXT_URL: 'http://localhost:3000',
         VERCEL_URL: 'https://anime-vs.vercel.app',
         DATABASE_URL: 'postgresql://postgres:bpivIECW0oepIuXq@db.tbxrlogvaaogawkgsazj.supabase.co:5432/postgres',
     },
-    async rewrites() {
+    async headers() {
         return [
             {
+                // matching all API routes
                 source: '/api/:path*',
-                destination: 'https://anime-vs.vercel.app/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
             },
         ];
     },
